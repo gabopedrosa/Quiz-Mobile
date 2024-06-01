85926395c763de579677899e4c6c3786b01b9c09
witalomelo <witalovmelo@gmail.com>
2024-06-01 13:11:18 -0300

    finalizando cadastro de obras

--- app/src/main/java/com/example/quizapp/adm/dao/ObrasDao.kt
+++ app/src/main/java/com/example/quizapp/adm/dao/ObrasDao.kt
@@ -20,9 +20,16 @@ class ObrasDao {
                 nome = "MonaLisa",
                 autor = "Leornado Da Vinci",
                 data = "Entre 1503 e 1506",
-                periodo = "Renascimento",
+                periodo = "Lorem ipsum dolor sit amet, consectetur adipiscing eliliquet nces dui",
                 imagem = "https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
-            )
+            ),
+            Obra(
+                nome = "O Pensador",
+                autor ="Auguste Rodin",
+                data = "1880",
+                periodo = "Lorem ipsum",
+                imagem = "https://images.pexels.com/photos/46174/strawberries-berries-fruit-freshness-46174.jpeg"
+            ),
         )
     }
 }
